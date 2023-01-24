a8f188466a315682a5e326072545b2082a93dfc8
camdixon132 <camdixon132@gmail.com>
2023-01-24 15:51:11 -0500

    First Commit

---
+++ MovieDatabaseLab/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabaseLab
+{
+	public class Movie
+	{
+		//properties
+		public string Title { get; set; }
+		public string Category { get; set; }
+
+		//constructors
+		public Movie(string _title, string _category)
+		{
+			Title = _title;
+			Category = _category;
+		}
+
+		//methods
+		public string MovieTitle()
+		{
+			return Title;
+		}
+		public string MovieCategory()
+		{
+			return Category;
+		}
+	}
+}
+
---
+++ MovieDatabaseLab/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using MovieDatabaseLab;
+
+bool runProgram = true;
+
+List<Movie> AllMovies = new List<Movie>()
+{
+    new Movie("Midsommar", "Thriller"),
+    new Movie("The Big Lebowski", "Comedy"),
+    new Movie("No Country for Old Men", "Drama"),
+    new Movie("Ray", "Biopic"),
+    new Movie("Requiem For a Dream", "Drama"),
+    new Movie("Star Wars III", "Scifi"),
+    new Movie("American Psycho", "Thriller"),
+    new Movie("The Dark Knight", "Action"),
+    new Movie("Blade Runner 2049", "Scifi"),
+    new Movie("Django", "Drama")
+};
+
+
+
+
+Console.WriteLine("Welcome to the Movie List Application!");
+Console.WriteLine("There are 15 movies in this list.");
+
+
+while (runProgram == true)
+{
+    Console.WriteLine("What category are you interested in?");
+    string choice = Console.ReadLine();
+
+    foreach (Movie m in AllMovies)
+    {
+        if(m.Category == choice)
+        {
+            Console.WriteLine(m.Title, m.Category);
+        }
+    }
+
+
+
+
+
+
+
+
+
+    Console.WriteLine("Continue? y/n");
+    string again = Console.ReadLine();
+    if (again == "y" || again == "yes")
+    {
+        runProgram = true;
+    }
+    else
+    {
+        Console.WriteLine("Goodbye");
+        runProgram = false;
+    }
+
+}
+
+
+
+
+
+
+
+
